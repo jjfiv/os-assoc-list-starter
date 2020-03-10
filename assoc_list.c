652429a8e6d80c6907fab392766a49413ee747b0
Ella Chang <echang33@smith.edu>
2020-03-10 14:34:51 -0400

    finished

--- assoc_list.c
+++ assoc_list.c
@@ -16,12 +16,42 @@ void assoc_put(AssocList_t* self, bytes_t* key, bytes_t* value) {
 	assert(key != NULL);
 	assert(value != NULL);
 
-	// TODO: finish this method.
 	printf("called assoc_put: ");
-	bytes_print(stdout, key);
-	printf(" ");
-	bytes_print(stdout, value);
-	printf("\n");
+        bytes_print(stdout, key);
+        printf(" ");
+        bytes_print(stdout, value);
+        printf("\n");
+	
+	// if the list is empty, create a new head entry
+	if (self->head == NULL) {
+	  AssocEntry_t* new_head = malloc(sizeof(AssocEntry_t));
+          new_head->key = key;
+          new_head->value = value;
+          new_head->next = NULL;
+	  self->head = new_head;
+	  return;
+	}
+
+	// if the key already exists in the list, override the old entry
+	AssocEntry_t* curr = self->head;
+	while (curr != NULL) {
+	  if (bytes_equal(curr->key, key)) {
+	    curr->value = value;
+	    return;
+	  }
+	  curr = curr->next;
+	}
+
+	// otherwise, create a new entry and add it to the head of the list
+	AssocEntry_t* entry = malloc(sizeof(AssocEntry_t));
+        entry->key = key;
+        entry->value = value;
+        entry->next = NULL;
+	AssocEntry_t* old_head = self->head;
+	entry->next = old_head;
+	self->head = entry;
+	
+	self->num_elements += 1;
 }
 
 // Remove an entry matching this key in the association list.
@@ -29,7 +59,28 @@ void assoc_remove(AssocList_t* self, bytes_t* key) {
 	assert(self != NULL);
 	assert(key != NULL);
 
-	// TODO: finish this method.
+	// traverse the list while keeping track of the matching entry and the entry before it
+	AssocEntry_t* curr = self->head;
+	AssocEntry_t* prev = NULL;
+	while (curr != NULL && !bytes_equal(curr->key, key)) {
+	  prev = curr;
+	  curr = curr->next;
+	  if (curr == NULL) printf("%s", "the key doesn't exist");
+	}
+
+	// if the head entry has the matching key, update the head entry
+	if (prev == NULL) {
+	  self->head = curr->next;
+	  free(curr);
+
+	// otherwise update the matching entry
+	} else {
+	  prev->next = curr->next;
+	  free(curr);
+	}
+
+	self->num_elements -= 1;
+	
 	printf("called assoc_remove: ");
 	bytes_println(stdout, key);
 }
@@ -43,6 +94,13 @@ bytes_t* assoc_get(AssocList_t* self, bytes_t* key) {
 	bytes_println(stdout, key);
 
 	// TODO: finish this method; return NULL if missing!
+	AssocEntry_t* curr = self->head;
+	while (curr != NULL) {
+	  if (bytes_equal(curr->key, key)) {
+	    return curr->value;
+	  }
+	  curr = curr->next;
+	}
 	return NULL;
 }
 
